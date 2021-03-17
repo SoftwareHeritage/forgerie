9d75113c63c8c246bfd761033c9c719c0079793f
Frank Duncan <frank@kank.net>
2021-03-17 14:52:23 -0500

    Small updates to bin directory
    Removing project specific script, as well as small documentation
    additions.

--- bin/getdetails.sh
+++ bin/getdetails.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Gets the details of a commit for a given repository, in md5sum form.
+# This is useful to see if two commits, from two different repositories, represent
+# the same diff.  The primary use case for this is checking when a phabricator
+# commit in its staging repository matches a commit in the main repository, in order
+# to build a differential chain.  Because the staging repository has a lot more commits,
+# the index is usually more characters, meaning we need to grep out the "^index" lines
+# in order to build hashes that do match.
+
 COMMIT_ID=$1
 REPOSITORY_LOCATION=$2
 
