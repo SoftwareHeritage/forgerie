1b3d150274e370e434efbc85bca8d925757e7db3
Nicolas Dandrimont <nicolas@dandrimont.eu>
2022-10-17 12:18:13 +0200

    Don't force push on the master branch, which is protected

--- src/main/gitlab/export.lisp
+++ src/main/gitlab/export.lisp
@@ -1247,7 +1247,8 @@
            (delete-file patch-file))))))
       (forgerie-core:merge-request-changes mr))
      (git-cmd project "push" "-f" "gitlab" (forgerie-core:branch-name (forgerie-core:merge-request-source-branch mr)))
-     (git-cmd project "push" "-f" "gitlab" (forgerie-core:branch-name (forgerie-core:merge-request-target-branch mr)))
+     (when (string/= "master" (forgerie-core:branch-name (forgerie-core:merge-request-target-branch mr)))
+      (git-cmd project "push" "-f" "gitlab" (forgerie-core:branch-name (forgerie-core:merge-request-target-branch mr))))
      (update-mapping (:merge-request (forgerie-core:merge-request-id mr))
       (let
        ((gl-mr
